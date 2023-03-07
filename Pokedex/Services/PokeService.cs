5cd10b87a28255b84d4059c762d7708f76e39f3e
Mariah <livimariahzaratini@gmail.com>
2023-03-07 12:28:43 -0300

    Finalização da classe de serviços

--- Pokedex/Services/PokeService.cs
+++ Pokedex/Services/PokeService.cs
@@ -1,47 +1,52 @@
 namespace Pokedex.Services;
 public class PokeService : IPokeService
 {
-    private  readonly IHttpContextAccessor _session;
+    private readonly IHttpContextAccessor _session;
 
     public PokeService(IHttpContextAccessor session)
     {
         _session = session;
+        PopularSessao();
     }
-    Pokemon GetPokemon(int Numero)
+    public Pokemon GetPokemon(int Numero)
     {
-
+        PopularSessao();
+        var pokemons = JsonSerializer.Deserialize<List<Pokemon>>(_session.HttpContext.Session.GetString("Pokemons"));
+        return pokemons.Where(pokemons => pokemons.Numero == Numero).FirstOrDefault();
     }
-    List<Pokemon> GetPokemons()
+    public List<Pokemon> GetPokemons()
     {
-
+        PopularSessao();
+        var pokemons = JsonSerializer.Deserialize<List<Pokemon>>(_session.HttpContext.Session.GetString("Pokemons"));
+        return pokemons;
     }
-    List<Tipo> GetTipos()
+    public List<Tipo> GetTipos()
     {
-
+        PopularSessao();
+        var tipos = JsonSerializer.Deserialize<List<Tipo>>(_session.HttpContext.Session.GetString("Tipos"));
+        return tipos;
     }
 }
 
 
 
-    private void PopularSessao()
+private void PopularSessao()
+{
+    if (string.IsNullOrEmpty(_session.HttpContext.Session.GetString("Pokemons")))
     {
-        //  ViewData["Tipos"] = JsonSerializer.Deserialize<List<Tipo>>(
-        //     LerArquivo(@"Data\tipos.json")
-        // );
-        // var dados = LerArquivo(@"Data\pokemons.json");
-        // var pokemons = JsonSerializer.Deserialize<List<Pokemon>>(dados);
         var dados = LerArquivo(@"Data\pokemons.json");
         _session.HttpContext.Session.SetString("Pokemons", dados);
         dados = LerArquivo(@"Data\pokemons.json");
         _session.HttpContext.Session.SetString("Tipos", dados);
     }
+}
 
 private string LerArquivo(string nomeArquivo)
+{
+    using (StreamReader leitor = new StreamReader(nomeArquivo))
     {
-        using (StreamReader leitor = new StreamReader(nomeArquivo))
-        {
-            string dados = leitor.ReadToEnd();
-            return dados;
-        }
+        string dados = leitor.ReadToEnd();
+        return dados;
     }
+}
 
